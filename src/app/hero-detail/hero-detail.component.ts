e936d0699b671bdf1431fbf891bf838eb0732442
ch <mkmsacc@outlook.com>
2021-04-06 16:21:00 +0700

    Finish 6. get data from a server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Hero} from '../hero';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
+
+import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
 
 @Component({
@@ -10,19 +11,20 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-  @Input() hero?: Hero;
-
-  constructor(private route: ActivatedRoute,
-              private heroService: HeroService,
-              private location: Location) {
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {
   }
 
   ngOnInit(): void {
     this.getHero();
   }
 
-  private getHero(): void {
+  getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
